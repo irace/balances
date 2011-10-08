634f4be9b5ec8fb5bfda298ec2eced68ff6f9a5b
Bryan Irace <bryan.irace@gmail.com>
2011-10-08 18:23:11 -0400

    Add Facebook login button

--- web.js
+++ web.js
@@ -1,11 +1,16 @@
 var app = require('express').createServer();
+app.register('.html', require('ejs')); // Files ending with ".html" should use EJS templating engine
 
-app.get('/', function(req, res){
-  res.send('Hello, debts!');
+app.set('view options', {
+    layout: false // Don't use a layout for now
+});
+
+app.get('/', function(request, response) {
+    response.render(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
